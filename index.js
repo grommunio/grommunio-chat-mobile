fe4a9738791600153906931d181d4b56542dba64
Stefan Akie <stefan.akie@grammm.com>
2021-12-16 15:43:56 +0100

    Exchange 'Mattermost' in translations

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2015-present grommuniochat, Inc. All Rights Reserved.
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
 import 'react-native/Libraries/Core/InitializeCore';

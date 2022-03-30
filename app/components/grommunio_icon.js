ae91d6eca69ce6236a9319a4f353b5b4dd22603e
Stefan Akie <stefan.akie@grammm.com>
2022-03-30 10:32:23 +0200

    Relabel app to grommunio-chat
    Change text of about dialog
    
    Change bundle identifier to grommunio
    
    Update ios icons
    
    Change ios icons to white background and remove redundant ones
    
    Exchange logos with grammm logos
    
    Relabel about dialog
    
    Change system message profile picture to grammm logo
    
    Update ToS and privacy links
    
    Remove dash from ios display name
    
    Remove dash from android app name
    
    Change bundle identifier
    
    Exchange logos with transparent ones
    
    Exchange icon with more centralized version
    
    Change example URL
    
    Change example urls to https://example.com/chat
    
    Remove grammm
    
    Rename package to chat.grommunio.com
    
    Sync ios folder from g-c-master branch
    
    Resolve all rebase errors
    
    Upgrade to targetSdkVersion 30
    
    Exchange new system message component
    
    Rename grammm to grommunio
    
    Exchange 'Mattermost' in translations

---
+++ app/components/grommunio_icon.js
@@ -0,0 +1,23 @@
+/* eslint-disable header/header */
+// Copyright (c) 2021-present grommunio GmbH. All Rights Reserved.
+
+import PropTypes from 'prop-types';
+import React from 'react';
+import {View, Image} from 'react-native';
+
+import Icon from '@assets/images/grommunio_favicon-triangle_light_512.png';
+
+const GrommunioIcon = ({style}) => (
+    <View>
+        <Image
+            source={Icon}
+            style={style}
+        />
+    </View>
+);
+
+GrommunioIcon.propTypes = {
+    style: PropTypes.object.isRequired,
+};
+
+export default GrommunioIcon;

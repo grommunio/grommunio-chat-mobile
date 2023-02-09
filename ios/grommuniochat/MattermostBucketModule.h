09289f9c4f6b73d738a6d0107ab948b43c2b376f
Stefan Akie <stefan.akie@grammm.com>
2023-02-09 15:39:03 +0100

    Das ist eine Kombination aus 5 Commits.
    Fix ios
    
    Fix build
    
    Change app name
    
    Exchange icons
    
    Exchange icons

--- ios/grommuniochat/MattermostBucketModule.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-#import "React/RCTBridgeModule.h"
-#import "MattermostBucket.h"
-
-@interface MattermostBucketModule : NSObject<RCTBridgeModule>
-@property MattermostBucket *bucket;
-@end

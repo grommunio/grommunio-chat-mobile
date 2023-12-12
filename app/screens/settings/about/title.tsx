bd1d8cf433d284d15e32881269a8bca29b81b29c
Stefan Akie <stefan.akie@grommunio.com>
2023-12-12 15:03:16 +0100

    Exchange about dialog icon

--- app/screens/settings/about/title.tsx
+++ app/screens/settings/about/title.tsx
@@ -4,9 +4,7 @@
 import React from 'react';
 import {Text} from 'react-native';
 
-import FormattedText from '@components/formatted_text';
 import {useTheme} from '@context/theme';
-import {t} from '@i18n';
 import {makeStyleSheetFromTheme} from '@utils/theme';
 import {typography} from '@utils/typography';
 
@@ -28,24 +26,10 @@ const getStyleSheet = makeStyleSheetFromTheme((theme) => {
 
 type TitleProps = {
     config: ClientConfig;
-    license: ClientLicense;
 }
-const Title = ({config, license}: TitleProps) => {
+const Title = ({config}: TitleProps) => {
     const theme = useTheme();
     const style = getStyleSheet(theme);
-
-    let id = t('about.teamEditiont0');
-    let defaultMessage = 'Team Edition';
-
-    if (config.BuildEnterpriseReady === 'true') {
-        id = t('about.teamEditiont1');
-        defaultMessage = 'Enterprise Edition';
-
-        if (license.IsLicensed === 'true') {
-            id = t('about.enterpriseEditione1');
-            defaultMessage = 'Enterprise Edition';
-        }
-    }
 
     return (
         <>
@@ -55,12 +39,6 @@ const Title = ({config, license}: TitleProps) => {
             >
                 {`${config.SiteName} `}
             </Text>
-            <FormattedText
-                id={id}
-                defaultMessage={defaultMessage}
-                style={[style.title, style.spacerBottom]}
-                testID='about.title'
-            />
         </>
 
     );
